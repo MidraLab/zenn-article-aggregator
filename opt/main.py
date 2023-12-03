7853472a6e834571c2ce46185d9b8d0fefb75812
yousan <ka1357amnbpdr@gmail.com>
2023-12-03 18:31:32 +0900

    fix: driverを使わない処理に置き換え

--- opt/main.py
+++ opt/main.py
@@ -1,39 +1,35 @@
+import asyncio
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import os
 import sys
 
 from zenn_scraper import ZennScraper
 from notion_manager import NotionManager
 
-if __name__ == "__main__":
+
+async def main():
     load_dotenv()
 
     notion_api_key = os.environ["MIDRA_LAB_NOTION_API"]
-    notion_database_id = os.environ["NOTION_DATABASE_URL"]
+    notion_database_id = os.environ["NOTION_DATABASE_ID"]
+    # TODO: 使用者によって変更する
+    usernames = ["keisuke114", "ayousanz"]
 
     publication_url = "https://zenn.dev/p/midra_lab"
 
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--log-level=3')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
-
     zenn_scraper = ZennScraper(publication_url)
-    zenn_scraper.get_articles(driver)
+    zenn_scraper.get_midra_lab_articles(usernames)
 
     if zenn_scraper.is_articles_empty():
-        driver.quit()
+        print("記事がありません。")
         sys.exit()
 
     notion_manager = NotionManager(notion_api_key, notion_database_id)
     notion_manager.delete_all_pages()
 
     for article in zenn_scraper.articles:
-        details = zenn_scraper.get_article_details(driver, article)
-        notion_manager.add_article(article['title'], article['url'], details['tags'], article['name'], details['date'])
+        notion_manager.add_article(article['title'], article['url'], article['name'], article['created_at'])
 
-    driver.quit()
+
+# 非同期イベントループを使用してmain関数を実行
+asyncio.run(main())
